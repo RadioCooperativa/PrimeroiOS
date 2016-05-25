5f23d118bd84e6e75bfe12313c17b9e87103a324
Rodrigo Vargas <rvargas@cooperativa.cl>
2016-05-25 18:39:17 -0400

    primer comiteo de codigo en xCode, se crea una clase prueba, generando con esto 2 archivos prueba.h prueba.m

---
+++ Primero/prueba.h
@@ -0,0 +1,13 @@
+//
+//  prueba.h
+//  Primero
+//
+//  Created by Innovación y Desarrollo on 25-05-16.
+//  Copyright © 2016 Innovación y Desarrollo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface prueba : NSObject
+
+@end
