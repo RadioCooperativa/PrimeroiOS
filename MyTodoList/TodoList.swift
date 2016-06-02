ab0348faa83bd136ee9fd3958aa7d8d738aa562a
Rodrigo Vargas <rvargas@cooperativa.cl>
2016-06-02 18:04:12 -0400

    Se modifica aplicacion de registro de tareas, agregando la funcionalidad de persistencia en disco, es decir registra en disco lo ingresado

--- MyTodoList/TodoList.swift
+++ MyTodoList/TodoList.swift
@@ -13,10 +13,68 @@ class TodoList: NSObject {
     //creando el modelo
     
     var items: [String] = []
+    
+    override init() {
+        super.init()
+        loadItems()
+    }
+    
+    private let fileURL: NSURL = { //tipo que guarda urls
+        /*let fileManager = NSFileManager.defaultManager()
+        let documentDirectoryURLs = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+        let documentDirectoryURL = documentDirectoryURLs.first!
+        
+        
+       // return documentDirectoryURL.URLByAppendigPathComponent("todolist.items")
+        //return documentDirectoryURL.URLByAppendigPathComponent("todolsit")*/
+        
+        
+        let documentDirectory = try! NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .LongStyle
+        dateFormatter.timeStyle = .ShortStyle
+        let date = dateFormatter.stringFromDate(NSDate())
+        let saveURL = documentDirectory.URLByAppendingPathComponent("list.items") // now it's NSURL
+        print ("path de documents \(saveURL)")
+        return saveURL
+    }()
+    
+    
     func addItem(item: String){
     items.append(item)
+        saveItems()
     }
+    
+    // codigo para guardar en disco
+    //debemos obtener la ruta del directorio Documents
+    //que es en donde podemos escribir.
+    
+    //clase de objetiveC NSFileManager
+    
+    func saveItems(){
+    //serializar es transformarse a bytes
+        
+        let itemsArray = items as NSArray
+        if itemsArray.writeToURL(self.fileURL, atomically: true){
+        print ("guardado")
+        
+        }else {
+        print ("no guardado")
+        }
+    }
+    func loadItems() {
+        
+        if let itemsArray = NSArray(contentsOfURL: self.fileURL) as? [String]{
+            
+            self.items = itemsArray
+            
+        }
+    }
+    
+    
 }
+
+
 
 extension TodoList: UITableViewDataSource {
 
