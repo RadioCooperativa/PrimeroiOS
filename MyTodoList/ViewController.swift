8ed6af5414828446bbe4aae908708830b422dfe7
Rodrigo Vargas <rvargas@cooperativa.cl>
2016-06-03 17:34:05 -0400

    UITextField avanzado, se crea el metodo para limitar la cantidad de texto contenido en el textfield

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var itemTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
     let todoList = TodoList()
-    
+    static let MAX_TEXT_SIZE = 5
     
     @IBAction func addButtonPressed (sender: UIButton){
     print ("Agregando un elemento a la lista : \(itemTextField.text)")
         todoList.addItem(itemTextField.text!) //con el signo ! sacamos el valor de la caja de texto
         tableView.reloadData()//cada ves que agregue un elemento el tableview actualice la informacion
+        self.itemTextField?.text = nil //borar campo de texto
         self.itemTextField?.resignFirstResponder()
         
     }
@@ -31,8 +32,9 @@ class ViewController: UIViewController, UITableViewDelegate {
     //cuando se esta inciiando la aplicacion se manda a llamar esta funcion
         
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell") //"Cell", debe estar escrito igual como quedo en el modelo, (let cell= tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath))
-    tableView.dataSource=todoList //a este objeto se le pregunta cuantas celdas vamos a agregar
-   tableView.delegate = self
+        tableView.dataSource=todoList //a este objeto se le pregunta cuantas celdas vamos a agregar
+        tableView.delegate = self //indicamos al tableview que somos su delegado o ayudante con su metodo
+        itemTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,6 +46,18 @@ class ViewController: UIViewController, UITableViewDelegate {
     //MARK: Metodos del table view delegate
     func scrollViewDidScroll(scrollView: UIScrollView) {
         self.itemTextField?.resignFirstResponder()
+    }
+    
+    //MARK: Metodos de text field delegate
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool{
+    
+        if let tareaString = textField.text as? NSString {
+            let updatedString = tareaString.stringByReplacingCharactersInRange(range, withString: string)
+            return updatedString.characters.count <= ViewController.MAX_TEXT_SIZE //regresa un booleano true
+        }else {
+        
+        return true
+        }
     }
 
 
